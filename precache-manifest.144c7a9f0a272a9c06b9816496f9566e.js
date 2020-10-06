4a4045f2d71ac064fa354ccd2aca0a1d304d696b
Andrei Radulescu <andreiradulescu94@gmail.com>
2020-10-06 12:54:33 +0300

    Updates

---
+++ precache-manifest.144c7a9f0a272a9c06b9816496f9566e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d102d385d5868ce6896e7ce6cb4febae",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "6d5a5ae05c23788a7305",
+    "url": "/react-weather-app/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "69eeaae4e3bee3639b34",
+    "url": "/react-weather-app/static/css/main.9900741b.chunk.css"
+  },
+  {
+    "revision": "6d5a5ae05c23788a7305",
+    "url": "/react-weather-app/static/js/2.6b6e6aa0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-weather-app/static/js/2.6b6e6aa0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69eeaae4e3bee3639b34",
+    "url": "/react-weather-app/static/js/main.8d3633ef.chunk.js"
+  },
+  {
+    "revision": "aacb36d1c4af6b3eab1b",
+    "url": "/react-weather-app/static/js/runtime-main.d770b3fe.js"
+  }
+]);
